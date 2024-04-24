90dbfee53c5575180b61c308a760c09cc575427f
Trinh Minh Khanh <trinhminhkhanh278@gmail.com>
2024-04-24 14:55:47 +0700

    23d12

--- 2023/12.py
+++ 2023/12.py
@@ -1,156 +1,113 @@
-symbols = "#.?"
+def df_search(record: str, groups: tuple[int], index=0) -> list[str]:
+    if not groups:
+        if "#" in record[index:]:
+            return []
+        return [record.replace("?", ".")]
+    index = (
+        [index + i for i, spring in enumerate(record[index:]) if spring != "."]
+        or [len(record)]
+    )[0]
+    if index + sum(groups) + len(groups) - 2 >= len(record):
+        return []
+
+    group_length = groups[0]
+    end_i = index + group_length
+    arrangements: list[tuple[str, tuple[int], int]] = []
+
+    if (
+        end_i <= len(record)
+        and (index == 0 or record[index - 1] != "#")
+        and not "." in record[index:end_i]
+        and (end_i == len(record) or record[end_i] != "#")
+    ):
+        new_record = list(record)
+        if index >= 1:
+            new_record[index - 1] = "."
+        new_record[index:end_i] = ["#"] * group_length
+        if end_i < len(record):
+            new_record[end_i] = "."
+        arrangements.append(("".join(new_record), groups[1:], end_i + 1))
+
+    if record[index] == "?":
+        new_record = list(record)
+        new_record[index] = "."
+        arrangements.append(("".join(new_record), groups, index + 1))
+
+    result: list[str] = []
+    for new_arrangement, new_groups, new_index in arrangements:
+        result.extend(df_search(new_arrangement, new_groups, new_index))
+
+    return result
+
+
+def find_combinations(record: str, groups: tuple[int]):
+    first, last = record.find("#"), record.rfind("#")
+    if first == -1:
+        first = len(record)
+    group_placements: list[list[int]] = [[] for _ in groups]
+    for group_i, prev_length in enumerate(groups):
+        for record_i in range(
+            sum(groups[:group_i]) + group_i,
+            len(record) - (sum(groups[group_i:]) + len(groups) - group_i - 1) + 1,
+        ):
+            end_i = record_i + prev_length
+            if (
+                not (group_i == 0 and first < record_i)
+                and not (group_i == len(groups) - 1 and end_i <= last)
+                and (record_i == 0 or record[record_i - 1] != "#")
+                and not "." in record[record_i:end_i]
+                and (end_i == len(record) or record[end_i] != "#")
+            ):
+                group_placements[group_i].append(record_i)
+
+    combinations = [[1 for _ in group_placements[-1]]]
+    for prev_placements, prev_length, next_placements in zip(
+        group_placements[-2::-1],
+        groups[-2::-1],
+        group_placements[:0:-1],
+    ):
+        combinations.insert(
+            0,
+            [
+                sum(
+                    next_count
+                    for next_i, next_count in zip(next_placements, combinations[0])
+                    if prev_i + prev_length < next_i
+                    and not "#" in record[prev_i + prev_length + 1 : next_i]
+                )
+                for prev_i in prev_placements
+            ],
+        )
+
+    return combinations
 
 
 def run(input_data: str):
-    part1 = 0
-    part2 = 0
-    for i, line in enumerate(input_data.splitlines()):
-        split = line.split()
-        records = list(split[0])
-        groups = [int(i) for i in split[1].split(",")]
-        arrangements = find_arrangements(records, groups)
+    part1, part2 = 0, 0
+
+    input_lines = input_data.splitlines()
+    # output = []
+    for line in input_lines:
+        record, groups = tuple((line.split()))
+        groups = tuple(int(g) for g in groups.split(","))
+        arrangements = df_search(record, groups)
         part1 += len(arrangements)
 
-        # new_records = [list(records) for _ in (0, 1)]
-        # new_records[0].append("?")
-        # new_records[1].insert(0, "?")
-        # new_arrangements_list = [
-        #     find_arrangements(record, groups) for record in new_records
-        # ]
-        # a = 0 if len(new_arrangements_list[0]) >= len(new_arrangements_list[1]) else 1
-        # new_arrangements = new_arrangements_list[a]
-        # mismatch = len([arr for arr in arrangements if arr[-a] == "#"]) * len(
-        #     [arr for arr in new_arrangements if arr[a - 1] == "#"]
-        # )
-        # total_arr = len(arrangements) * int(
-        #     math.pow(len(new_arrangements) - mismatch, 4)
-        # )
-        # part2 += total_arr
+        # combinations = find_combinations(record, groups)
+        combinations = find_combinations("?".join([record] * 5), groups * 5)
+        part2 += sum(combinations[0])
 
-        records.append("?")
-        records2 = (records * 5)[:-1]
-        groups2 = groups * 5
-
-        placable_list = []
-        i = 0
-        while i < len(records2):
-            if records2[i] != ".":
-                for ii in range(i, len(records2)):
-                    if ii == len(records2) - 1 or records2[ii + 1] == ".":
-                        placable_list.append((i, ii))
-                        i = ii
-                        break
-            i += 1
-        groups_ranges = [[0]]
-        for group in groups2[:-1]:
-            groups_ranges.append([groups_ranges[-1][0] + group + 1])
-        groups_ranges[-1].append(len(records2) - groups2[-1])
-        for g, group in enumerate(groups2[-2::-1]):
-            groups_ranges[-2 - g].append(groups_ranges[-1 - g][1] - group - 1)
-
-        group_placables = []
-        for group, group_range in zip(groups2, groups_ranges):
-            group_placables.append([])
-            for placable in placable_list:
-                if group_range[1] < placable[0]:
-                    break
-                min_start = max(group_range[0], placable[0])
-                max_start = min(group_range[1], placable[1])
-                if min_start <= max_start:
-                    group_placables[-1].extend(range(min_start, max_start + 1))
-        arrangements2 = []
-        for group, gp1, gp2 in zip(
-            groups2[:-1], group_placables[:-1], group_placables[1:]
-        ):
-            arrangements2.append([])
-            # cache = {}
-            for g1 in gp1:
-                index_valid = len(gp2)
-                for i2, g2 in enumerate(gp2):
-                    if g1 + group < g2:
-                        index_valid = i2
-                        break
-                # if index_valid in cache:
-                #     arr_count = cache[index_valid]
-                # else:
-                #     arr_count = sum(arrangements2[-2][:index_valid])
-                #     cache[index_valid] = arr_count
-                if index_valid != len(gp2):
-                    arrangements2[-1].append(index_valid)
-        # for g in range(len(arrangements2) - 2, -1, -1):
-        #     delete = len(arrangements2[g])
-        #     for a, arr in enumerate(arrangements2[g]):
-        #         if arr >= len(arrangements2[g + 1]):
-        #             delete = a
-        #             break
-        #     arrangements2[g] = arrangements2[g][:delete]
-
-        arrangements2_count = sum(arrangements2[-1])
-
-        arrangements_test = find_arrangements(records2, groups2)
-        test = [{} for g in groups2]
-        for arr in arrangements_test:
-            for i, index in enumerate(arr):
-                if index in test[i]:
-                    test[i][index] += 1
-                else:
-                    test[i][index] = 1
-
-        part2 += len(arrangements_test)
-
-        # for i in range(len(records2)):
-        #     print((i + 1) % 10, end="")
-        # print()
-        # print("".join(records2) + "  " + ",".join([str(g) for g in groups2]))
-        # # for arr in arrangements_test[:100]:
-        # #     print(arr)
-        # print()
+    #     if len(arrangements) != sum(combinations[0]):
+    #         output.extend(
+    #             [
+    #                 "".join(str(i % 10) for i, _ in enumerate(record)),
+    #                 f"{record} {groups}",
+    #                 "\n".join(arrangements),
+    #                 "\n".join(str(gp) for gp in group_placements),
+    #                 "\n".join(str(a) for a in combinations),
+    #                 f"{len(arrangements)}|{sum(combinations[0])}",
+    #             ]
+    #         )
+    # with open("output", "w") as file:
+    #     [print(line, file=file) for line in output]
     return part1, part2
-
-
-def find_arrangements(arrangement, groups, index=0, starts=[]):
-    if len(groups) == 0:
-        for c, s in enumerate(arrangement[index + 1 :]):
-            if s == "?":
-                arrangement[index + 1 + c] = "."
-            elif s == "#":
-                return []
-        return [starts]
-    if index == len(arrangement):
-        return []
-
-    if arrangement[index] == ".":
-        for i, s in enumerate(arrangement[index + 1 :]):
-            if s != ".":
-                index += i + 1
-                break
-        if index == len(arrangement) - 1 and arrangement[index] == ".":
-            index += 1
-        new_arrangements_list = [[arrangement, groups, index, starts]]
-    elif arrangement[index] == "#":
-        end = index + groups[0]
-        if end > len(arrangement) or (
-            end < len(arrangement) and arrangement[end] == "#"
-        ):
-            return []
-        for s in range(index + 1, index + groups[0]):
-            if arrangement[s] == ".":
-                return []
-            if arrangement[s] == "?":
-                arrangement[s] = "#"
-        if end < len(arrangement):
-            arrangement[end] = "."
-        starts.append(index)
-        new_arrangements_list = [[arrangement, groups[1:], end + 1, starts]]
-    else:
-        new_arrangements_list = [
-            [list(arrangement), groups, index, list(starts)] for a in range(2)
-        ]
-        for a in (0, 1):
-            new_arrangements_list[a][0][index] = symbols[a]
-
-    result = []
-    for arr in new_arrangements_list:
-        # print(("".join(arr[0]), arr[1], arr[2], arr[3]))
-        result.extend(find_arrangements(arr[0], arr[1], arr[2], arr[3]))
-    return result
